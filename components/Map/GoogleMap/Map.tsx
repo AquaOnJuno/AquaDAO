bcecb7d86bc1b1f427cb6cfb5ffa5eb03aa04580
alenzer <alenzer0902@gmail.com>
2022-06-27 18:16:19 -0400

    remove google map

--- components/Map/GoogleMap/Map.tsx
+++ components/Map/GoogleMap/Map.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from "react";
-import GoogleMapReact from "google-map-react";
+// import GoogleMapReact from "google-map-react";
 // import { Link } from "react-router-dom";
 
 const markerStyle = {
@@ -26,14 +26,14 @@ const SimpleMap: FunctionComponent<Props> = ({
   return (
     // Important! Always set the container height explicitly
     <div style={{ height: "100vh", width: "100%" }}>
-      <GoogleMapReact
+      {/* <GoogleMapReact
         bootstrapURLKeys={{
           key: "AIzaSyA16d9FJFh__vK04jU1P64vnEpPc3jenec"
         }}
         defaultCenter={center}
         defaultZoom={zoom}
       >
-        {/* {locations.map((item: any) => {
+        {locations.map((item: any) => {
           if (item.address.length !== 0) {
             return item.address.map((i: any) => {
               return (
@@ -43,8 +43,8 @@ const SimpleMap: FunctionComponent<Props> = ({
               );
             });
           }
-        })} */}
-      </GoogleMapReact>
+        })}
+      </GoogleMapReact> */}
     </div>
   );
 }
