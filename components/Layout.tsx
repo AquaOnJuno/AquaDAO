1ba1519f8213b708aae160e917a7ca42e3f001e5
alenzer <alenzer0902@gmail.com>
2022-06-27 02:31:13 -0400

    add max width

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,18 +4,20 @@ import Navbar from "./Navbar";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen bg-base-100 text-base-content bg-black "
+    <div className="flex flex-col min-h-screen bg-base-100 text-base-content bg-black justify-center items-center"
       style={{ fontFamily: "Inter" }}
     >
       <Head>
         <title>{process.env.NEXT_PUBLIC_SITE_TITLE}</title>
-        <meta name="description" content="Bringing clean water to every person
-on the planet." />
+        <meta
+          name="description"
+          content="Bringing clean water to every person on the planet."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Navbar />
-      <div className="flex flex-col items-center justify-center w-full">
+      <div className="flex flex-col items-center justify-center max-w-screen-2xl">
         {children}
       </div>
       {/* <footer className="flex items-center justify-center w-full h-24">
